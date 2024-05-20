e6609ea9f2ab75eb02840f61d3696c18882b5794
NazWaz <wazidur123@gmail.com>
2024-05-20 19:18:01 +0100

    adds docstrings and cleans up code for database utils file

--- database_utils.py
+++ database_utils.py
@@ -1,52 +1,68 @@
-#%%
 import yaml    
 import sqlalchemy
 from sqlalchemy import inspect
 from sqlalchemy import text
 import psycopg2
 import pandas as pd
-
-#%%
 class DatabaseConnector:
+    '''
+    Connects with and uploads data to a postgresql database.
 
+    Methods:
+    -------
+    read_db_creds
+        Reads database credentials.
+    init_db_engine
+        Initialises sqlalchemy database engine.
+    list_db_tables
+        Lists all tables in database.
+    upload_to_db
+        Uploads data from dataframe to a table in a postgresql database.
+    '''
     def __init__(self):
         pass
-
-    # reads database credentials    
+  
     def read_db_creds(self):
+        '''
+        Reads a credential yaml file to return a dictionary of the database credentials.
+        '''
+
         with open("db_creds.yaml", "r") as db_creds:
             db_creds = yaml.safe_load(db_creds)
         db_creds = list(db_creds.values())
         return db_creds
         
-    # intialises sqlalchemy database engine
     def init_db_engine(self):
+        '''
+        Reads the database credentials to initialise and return a sqlalchemy database engine. 
+        '''
+
         db_creds = self.read_db_creds()
         HOST, PASSWORD, USER, DATABASE, PORT = [db_creds[i] for i in (0, 1, 2, 3, 4)] 
         source_engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
         return source_engine
 
-    # lists all tables in database
     def list_db_tables(self):
+        '''
+        Lists all the tables in the database using the sqlalchemy database engine.
+        '''
+
         source_engine = self.init_db_engine()
         inspector = inspect(source_engine)
         print(inspector.get_table_names())
 
-    # uploads data from pd to database in a table
     def upload_to_db(self, data,  table_name):
+        '''
+        Uploads data from dataframe to store in postgresql database using more database credentials from yaml file.
+        '''
+
         db_creds = self.read_db_creds()
-        
         HOST, PASSWORD, USER, DATABASE, PORT = [db_creds[i] for i in (5, 6, 7, 8, 9)] 
         sink_engine = sqlalchemy.create_engine(f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
         data.to_sql(table_name, sink_engine, if_exists="replace")
        
 if __name__ == "__main__":
     dbsconnector = DatabaseConnector()
-    #dbsconnector.read_db_creds()
-    #print(dbsconnector.read_db_creds())
-    #dbsconnector.init_db_engine()
-    #print(dbsconnector.init_db_engine())
-    #dbsconnector.list_db_tables()
     from data_cleaning import DataCleaning
     cleaning = DataCleaning()
 
@@ -73,5 +89,3 @@ if __name__ == "__main__":
     # uploads date events data
     clean_events_data = cleaning.clean_events_data()
     dbsconnector.upload_to_db(clean_events_data, "dim_date_times")
-
-# %%
